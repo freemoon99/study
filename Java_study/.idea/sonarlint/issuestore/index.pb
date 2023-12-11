5aa780069e2dd6f78bb53b53f1ee01d1f047f37f
freemoon99 <soloist132@gmail.com>
2023-12-11 19:07:27 +0900

    자바 ch1,ch2 강의 시청

---
+++ Java_study/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+=
+src/Main.java,9\2\924f089edb4af5ee45c8a9d1129cd28e2b368138
+T
+$src/ch1/자바기초/HellowJava.java,7\0\7018c0d96a9340cbd8c19b9b75cccafaf6257737
+S
+#src/ch1/자바기초/HelloJava.java,f\7\f7ea79f5c711b2846415fe8598bc23918ed83063
---
+++ Java_study/src/ch2/변수/problem.java
@@ -0,0 +1,27 @@
+package ch2.변수;
+
+public class problem {
+    public static void main(String[] args) {
+        /*// 문제 1
+        int num1 = 4, num2 = 3;
+        System.out.println(num1 + num2);
+        System.out.println(num1 - num2);
+        System.out.println(num1 * num2);
+        */
+
+        /*// 문제 2
+        int num1 = 10;
+        int num2 = 20;
+        int sum = num1 + num2;
+        System.out.println(sum);
+        */
+
+        /*// 문제 3
+        long a = 10000000000L;
+        System.out.println(a);
+
+        boolean b = true;
+        System.out.println(b);
+         */
+    }
+}
